6f9688ac3328451d697980b6d8a2507f23f45f49
Anmol <anmolgupta02@gmail.com>
2021-02-07 03:50:06 +0530

    Expense Section init.

--- lib/widgets/expenseSection.dart
+++ lib/widgets/expenseSection.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class ExpensesSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text("This is a text section"),
+    );
   }
 }
